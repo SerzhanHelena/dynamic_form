71c80dd43c3c68d744612fc41fdf7cdfecf6a3d9
SerzhanHelena <serzhan.helen@gmail.com>
2019-01-02 12:50:28 +0300

    added a dynamic form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild} from "@angular/core";
+import { Validators } from "@angular/forms";
+import { FieldConfig } from "./field.interface";
+import { DynamicFormComponent} from './components/dynamic_form/dynamic-form/dynamic-form.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
   title = 'dynamic-form';
+
+  regConfig: FieldConfig[] = [
+    {
+      type: "input",
+      label: "Username",
+      inputType: "text",
+      name: "name",
+      validations: [
+        {
+          name: "required",
+          validator: Validators.required,
+          message: "Name Required"
+        },
+        {
+          name: "pattern",
+          validator: Validators.pattern("^[a-zA-Z]+$"),
+          message: "Accept only text"
+        }
+      ]
+    },
+    {
+      type: "input",
+      label: "Email Address",
+      inputType: "email",
+      name: "email",
+      validations: [
+        {
+          name: "required",
+          validator: Validators.required,
+          message: "Email Required"
+        },
+        {
+          name: "pattern",
+          validator: Validators.pattern(
+            "^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$"
+          ),
+          message: "Invalid email"
+        }
+      ]
+    },
+    {
+      type: "date",
+      label: "Date",
+      name: "date",
+      validations: [
+        {
+          name: "required",
+          validator: Validators.required,
+          message: "Date of Birth Required"
+        }
+      ]
+    },
+    {
+      type: "select",
+      label: "Skill",
+      name: "skill",
+      value: "JS",
+      options: ["Angular", "React", "JS", "Html"]
+    },
+    {
+      type: "checkbox",
+      label: "Accept Terms",
+      name: "term",
+      value: true
+    },
+    {
+      type: "button",
+      label: "Save"
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { InputComponent } from './components/input/input.component';
+import { ButtonComponent } from './components/button/button.component';
+import { SelectComponent } from './components/select/select.component';
+import { DateComponent } from './components/date/date.component';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
+import { DynamicFieldDirective } from './components/dynamic_fields/dynamic-field.directive';
+import { DynamicFormComponent } from './components/dynamic_form/dynamic-form/dynamic-form.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    ButtonComponent,
+    SelectComponent,
+    DateComponent,
+    CheckboxComponent,
+    DynamicFieldDirective,
+    DynamicFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    InputComponent,
+    ButtonComponent,
+    SelectComponent,
+    DateComponent,
+    CheckboxComponent
+  ]
 })
 export class AppModule { }
